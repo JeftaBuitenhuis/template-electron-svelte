87d54e617d4336d19d84b03513ea03f514fe3486
JeftaBuitenhuis <Buitenhuis.jefta@gmail.com>
2024-06-04 21:10:31 +0200

    v0.0.0 - init

---
+++ svelte.config.js
@@ -0,0 +1,15 @@
+import adapter from '@sveltejs/adapter-static'
+
+export default {
+    kit: {
+        adapter: adapter({
+            // Output paths relative to the root directory
+            // Keep the folder structure by not changing the output paths
+			pages: './public',
+			assets: './public',
+            fallback: undefined,
+            precompress: true,
+            strict: true
+        })
+    }
+};
